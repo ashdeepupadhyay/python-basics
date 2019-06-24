63ae521f5506ff5fd95d8d2591c838569b625f08
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2019-06-24 16:20:42 +0530

    mad libs game

--- Drawing-shape/app.py
+++ Drawing-shape/app.py
@@ -1,4 +1,7 @@
-num1 = input("Enter a number : ")
-num2 = input("Enter a number : ")
-result = float(num1) + float(num2)
-print (result)
+color = input("Enter a Color :")
+plural_noun = input("Enter a plural noun")
+celebrity = input("Enter a Celebrity name")
+
+print("Roses are "+color)
+print(plural_noun+" are blue")
+print("I love "+celebrity)
