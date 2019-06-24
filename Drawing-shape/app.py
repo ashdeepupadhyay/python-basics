ca0903031bca0c272d9d012cb892b9afb6f0f048
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2019-06-24 19:35:21 +0530

    appending the file

--- Drawing-shape/app.py
+++ Drawing-shape/app.py
@@ -1,6 +1,8 @@
-employee_file=open("employee.txt","r")
-for employee in employee_file.readlines():
-    print(employee)
+employee_file=open("employee.txt","a")
+#for employee in employee_file.readlines():
+#    print(employee)
+
+employee_file.write("dart - HR\n")
 employee_file.close()
 
 
