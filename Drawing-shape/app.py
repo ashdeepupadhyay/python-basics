a3861b9ea6f2687dda5ee8fb48e4655f7fd70f8a
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2019-06-24 19:03:42 +0530

    2d list and matrix

--- Drawing-shape/app.py
+++ Drawing-shape/app.py
@@ -1,7 +1,10 @@
-def raise_to_power(base_num,pow_num):
-    result=1
-    for index in range(pow_num):
-        result = result * base_num
-    return  result
+number_grid = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [0]
+]
 
-print(raise_to_power(3,4))
+for row in number_grid:
+    for col in row:
+        print(col)
