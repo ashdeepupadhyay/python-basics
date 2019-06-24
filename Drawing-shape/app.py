9074d1423f507dd83b54e223f1b7115b9c753f80
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2019-06-24 20:20:52 +0530

    MCQ

--- Drawing-shape/app.py
+++ Drawing-shape/app.py
@@ -1,12 +1,28 @@
-from Student import Student
+from Question import Question
 
-student1 = Student("abc","Business",3.1,False)
-student2 = Student("Pam","Art",3.1,True)
+question_prompts = [
+    "What color are apples?\na)Red\nb)Yellow\nc)Blue\n\n",
+    "What color are Bananas?\na)Red\nb)Yellow\nc)Blue\n\n",
+    "What color are strawberries?\na)Red\nb)Yellow\nc)Blue\n\n"
+]
 
-print(student1.name)
-print(student1.gpa)
-print(student2.name)
 
+questions=[
+    Question(question_prompts[0],"a"),
+    Question(question_prompts[1],"b"),
+    Question(question_prompts[2],"a"),
+]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score+=1
+    print("You got"+str(score)+"/"+str(len(questions))+"Correct")
+
+
+run_test(questions)
 
 
 
