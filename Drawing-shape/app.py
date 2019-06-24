0f299606f8d022b92d791a86e963fcfac5cefb05
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2019-06-24 17:39:35 +0530

    making a guess game

--- Drawing-shape/app.py
+++ Drawing-shape/app.py
@@ -1,19 +1,16 @@
-monthConversions={
-    "Jan":"January",
-    "Feb":"February",
-    "Mar":"March",
-    "Apr":"April",
-    "May":"May",
-    "Jun":"June",
-    "Jul":"July",
-    "Aug":"August",
-    "Sept":"September",
-    "Oct":"October",
-    "Nov":"November",
-    "Dec":"December"
-}
+secret_word = "giraffe"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+while guess != secret_word and not(out_of_guesses):
+    if guess_count<guess_limit:
+        guess = input("Enter  guess: ")
+        guess_count+=1
+    else:
+        out_of_guesses = True
 
-print(monthConversions['Mar'])
-print(monthConversions.get('Dec'))
-print(monthConversions.get('lec','Not a valid key'))
-
+if out_of_guesses:
+    print("Out of Guesses,You Lose!")
+else:
+    print("You Win")
