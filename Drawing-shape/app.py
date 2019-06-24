0f97469a2df8e4195f190dd9c3b56044183c1fb4
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2019-06-24 20:26:26 +0530

    functions inside a class

--- Drawing-shape/app.py
+++ Drawing-shape/app.py
@@ -1,28 +1,11 @@
-from Question import Question
-
-question_prompts = [
-    "What color are apples?\na)Red\nb)Yellow\nc)Blue\n\n",
-    "What color are Bananas?\na)Red\nb)Yellow\nc)Blue\n\n",
-    "What color are strawberries?\na)Red\nb)Yellow\nc)Blue\n\n"
-]
+from Student import Student
 
 
-questions=[
-    Question(question_prompts[0],"a"),
-    Question(question_prompts[1],"b"),
-    Question(question_prompts[2],"a"),
-]
+student1=Student("abc","Accounting",3.1,False)
+student2=Student("zxc","Business",3.8,True)
 
-def run_test(questions):
-    score = 0
-    for question in questions:
-        answer = input(question.prompt)
-        if answer == question.answer:
-            score+=1
-    print("You got"+str(score)+"/"+str(len(questions))+"Correct")
-
-
-run_test(questions)
+print(student1.on_honor_roll())
+print(student2.on_honor_roll())
 
 
 
