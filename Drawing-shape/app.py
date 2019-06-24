fd0e86c83511a6254b491a9a860e9929c847eb79
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2019-06-24 20:06:43 +0530

    classes and objects

--- Drawing-shape/app.py
+++ Drawing-shape/app.py
@@ -1,6 +1,11 @@
-import usefulltools
+from Student import Student
 
-print(usefulltools.roll_dice(10))
+student1 = Student("abc","Business",3.1,False)
+student2 = Student("Pam","Art",3.1,True)
+
+print(student1.name)
+print(student1.gpa)
+print(student2.name)
 
 
 
